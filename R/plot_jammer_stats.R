78a95b36805a63bc4525c7d2c2768f87d0278d21
Biplabendu Das <biplabendu.das@gmail.com>
2024-08-28 22:07:22 -0700

    feat: add reset defaults btn, update jammer plots

--- R/plot_jammer_stats.R
+++ R/plot_jammer_stats.R
@@ -1,5 +1,37 @@
 plot_jammer_stats <- function(data, team_color) {
   data |> 
+    # transform
+    select(
+      team,
+      jammer,
+      n_jams,
+      percent_jams_lead,
+      percent_jams_penalty,
+      n_points_sum
+    ) |> 
+    tidyr::pivot_longer(
+      cols = matches("^n_|^percent_"),
+      names_to = "what",
+      values_to = "value"
+    ) |> 
+    mutate(
+      what = case_match(
+        what,
+        "n_jams" ~ "Number of Jams",
+        "n_points_sum" ~ "Total points (diff)",
+        "percent_jams_lead" ~ "Percent Lead",
+        "percent_jams_penalty" ~ "Percent Penalty"
+      ) |> 
+        factor(
+          levels = c(
+            "Number of Jams",
+            "Total points (diff)",
+            "Percent Lead",
+            "Percent Penalty"
+          )
+        )
+    ) |> 
+    # plot
     ggplot(
       aes(
         x = value,
@@ -14,16 +46,21 @@ plot_jammer_stats <- function(data, team_color) {
       nrow = 2,
       scales = "free_x"
     ) +
+    geom_vline(
+      xintercept = 0,
+      col = "red",
+      linewidth = 2
+    ) +
     geom_bar(
       stat = "identity"
     ) +
-    geom_point(
-      shape = 21,
-      col = "white",
-      size = 7,
-      alpha = 0.7,
-      stroke = 2
-    ) +
+    # geom_point(
+    #   shape = 21,
+    #   col = "white",
+    #   size = 7,
+    #   alpha = 0.7,
+    #   stroke = 2
+    # ) +
     labs(
       title = "",
       x = "",
@@ -34,9 +71,10 @@ plot_jammer_stats <- function(data, team_color) {
     #   breaks = scales::breaks_pretty()
     # ) +
     scale_x_continuous(
-      breaks = scales::breaks_pretty()
+      # breaks = scales::breaks_pretty(),
+      n.breaks = 4
     ) +
-    theme_bw(24) +
+    theme_minimal(30) +
     scale_fill_manual(
       # values = c("#B22EF0", "grey10")
       values = team_color
@@ -47,6 +85,10 @@ plot_jammer_stats <- function(data, team_color) {
     ) +
     theme(
       legend.position = "none",
-      legend.title = element_blank()
+      plot.title = element_blank(),
+      legend.title = element_blank(),
+      axis.title.y = element_blank(),
+      axis.text = element_text(size = 18),
+      axis.title.x = element_blank()
     )
 }
